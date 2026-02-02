10652293afaf1b3bc681faea40796bed50191ecf
Yazish1 <yazish87@gmail.com>
2026-02-02 11:25:20 +1100

    Automated bash testing for dash app (Final Task)

---
+++ test_script.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+source venv/Scripts/activate
+python -m pytest test_dom.py
+
+exit_code=$?
+
+if [ $exit_code -eq 0 ]; then
+    echo "Test Passed!"
+else
+    echo "Test failed"
+    exit 1
+fi
+
